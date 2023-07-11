e7113da0e0b02840036efe184e11b328bb93a4c0
gyojincompany <luckyred@gmail.com>
2023-07-12 01:12:28 +0900

    쇼핑몰 완성

---
+++ src/main/java/com/shop/shop/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.shop.shop.repository;
+
+import com.shop.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+
+}
